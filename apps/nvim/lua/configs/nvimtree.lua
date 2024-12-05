175ecd5af09fc16fe4d4536730e8f4687f5d087a
Raphael Castro <goose@Raphaels-MBP.lan>
2024-12-04 21:40:13 -0500

    working

--- apps/nvim/lua/configs/nvimtree.lua
+++ apps/nvim/lua/configs/nvimtree.lua
@@ -1,4 +1,6 @@
 dofile(vim.g.base46_cache .. "nvimtree")
+local HEIGHT_RATIO = 0.5  -- You can change this
+local WIDTH_RATIO = 0.3   -- You can change this too
 
 return {
   filters = { dotfiles = false },
@@ -9,9 +11,32 @@ return {
     enable = true,
     update_root = false,
   },
-  view = {
-    width = 30,
-    preserve_window_proportions = true,
+view = {
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = 'rounded',
+          relative = 'editor',
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
   },
   renderer = {
     root_folder_label = false,
